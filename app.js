d76d47daf97333761dde739d697543852f82120f
Rajesh <rk845280@gmail.com>
2019-02-24 21:34:37 +0530

    added some more changes and added records to MongoDB

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 const app = express();
 
@@ -8,13 +10,40 @@ const app = express();
 const db = require('./config/keys').MongoURI;
 
 //connect to Mongo
-mongoose.connect(db , {useNewUrlParser : true})
+mongoose.connect(db, {
+        useNewUrlParser: true
+    })
     .then(() => console.log("MongoDB connected"))
     .catch((err) => console.log(err));
 
 //EJS
 app.use(expressLayout);
 app.set('view engine', 'ejs');
+
+//Body parser
+app.use(express.urlencoded({
+    extended: false
+}));
+
+//Express session middleware
+app.use(
+    session({
+        secret: 'secret',
+        resave: true,
+        saveUninitialized: true
+    })
+);
+
+//connect flash
+app.use(flash());
+
+//global variables
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error = req.flash('error_msg');
+    next();
+})
+
 
 //Routes
 app.use('/', require('./routes/index'));
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,98 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+//User model
+const User = require('../models/User');
 
 //login page
 router.get('/login', (req, res) => res.render('login'));
 
 //register page
 router.get('/register', (req, res) => res.render('register'));
+
+//register handler
+router.post('/register', (req, res) => {
+    const {
+        name,
+        email,
+        password,
+        password2
+    } = req.body;
+    let errors = [];
+
+    //check required fields
+    if (!name || !email || !password || !password2) {
+        errors.push({
+            msg: 'please fill in all fields'
+        });
+    }
+
+    //check passwords match
+    if (password !== password2) {
+        errors.push({
+            msg: 'passwords do not match'
+        });
+    }
+
+    //password check
+    if (password.length < 6) {
+        errors.push({
+            msg: 'passwords mush contain atleast 6 characters'
+        });
+    }
+
+    //
+    if (errors.length > 0) {
+        res.render('register', {
+            errors,
+            name,
+            email,
+            password,
+            password2
+        });
+    } else {
+        //validation passed
+        User.findOne({
+                email: email
+            })
+            .then(user => {
+                if (user) {
+                    //user exists
+                    errors.push({
+                        msg: 'email is already registered'
+                    });
+                    res.render('register', {
+                        errors,
+                        name,
+                        email,
+                        password,
+                        password2
+                    });
+                } else {
+                    const newUser = new User({
+                        name,
+                        email,
+                        password
+                    });
+
+                    //Hash password
+                    bcrypt.genSalt(10, (err, salt) =>
+                        bcrypt.hash(newUser.password, salt, (err, hash) => {
+                            if (err) throw err;
+                            //set password to hashed
+                            newUser.password = hash;
+                            //save user
+                            newUser.save()
+                                .then(user => {
+                                    req.flash('success_msg', 'You are now register and now can log in');
+                                    res.redirect('/users/login');
+                                })
+                                .catch(err => console.log(err));
+                        }))
+                }
+            })
+    }
+})
 
 module.exports = router;
